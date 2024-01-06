2da05eef38ef23d1aea4dfd6a6c27140a1928f47
razor999920 <raza9999ali@gmail.com>
2024-01-06 02:40:30 -0700

    moving to kickstart and adding lazyvim

--- lua/core/colorscheme.lua
+++
@@ -1,9 +0,0 @@
--- Set Colorscheme
-local status, err = pcall(function()
-	vim.cmd("colorscheme gruvbox") -- onedark -- kanagawa -- kanagawa-dragon
-end)
-
-if not status then
-	print("Error setting colorscheme: " .. err)
-	return
-end
